12520eebf8b0321feadf4f4b9bccc19c219ee619
pius <piusrestiantoro2@gmail.com>
2023-05-31 18:17:11 +0800

    component update

--- frontend/src/components/welcome/welcome.jsx
+++ frontend/src/components/welcome/welcome.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import {useNavigate} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import "../welcome/welcome.css"
 
 function Welcome() {
-
-    const navigate = useNavigate()
-
-    const signup = ()=>{
-        navigate('/signup')
-    }
 
     return(
         <div className="welContainer">
@@ -18,7 +11,7 @@ function Welcome() {
                 <h1>Welcome to Online Notes App</h1>
                 <p>Your notes with you wherever you go.</p>
                 <p>Easy to use, protects all your notes!</p>
-                <button type="button" class="btn btn-lg signup btnColor" onClick={signup}>Sign up for free</button>
+                <button type="button" class="btn btn-lg getStarted btnColor" >Get Started</button>
             </div>
         </div>
     )
