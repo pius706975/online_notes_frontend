73788b6aaf45ea99034d9e7c5a6ca0317ae23cca
pius <piusrestiantoro2@gmail.com>
2023-06-14 18:20:09 +0800

    note preview updated

--- frontend/src/pages/notes/notePreview.jsx
+++ frontend/src/pages/notes/notePreview.jsx
@@ -13,19 +13,15 @@ function NotePreview() {
     const api = Api()
     const {isAuth} = useSelector((state)=>state.users)
     const params = useParams()
-    const [notePreview, setNotePreview] = useState([])
     const [title, setTitle] = useState("")
     const [note, setNote] = useState("")
-
-    const [data1, setData1] = useState("")
-    const [data2, setData2] = useState("")
-
+    
     const getPreview = ()=>{
         api.requests({
             url: '/note/' + params.id
         }).then((res)=>{
-            setData1(res.data.data.title)
-            setData2(res.data.data.note)
+            setTitle(res.data.data.title)
+            setNote(res.data.data.note)
 
             const {data} = res.data
             console.log(data);
@@ -38,18 +34,6 @@ function NotePreview() {
         getPreview()
     })
 
-    // const params = useParams()
-
-    // const getNotePreview = async ()=>{
-    //     try {
-    //         const {data: notePreview} = await axios.get(process.env.REACT_APP_BASEURL + '/note/' + params.id)
-    //         setNotePreview(notePreview.data)
-    //         console.log(notePreview.data);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
     return(
         <div className="preview-container">
             <NavbarCom/>
@@ -59,9 +43,9 @@ function NotePreview() {
                 {isAuth ? (
                     <div className="note-preview-container">
                         <h1>Note Preview</h1>
-                        <h3>{data1}</h3>
+                        <h3>{title}</h3>
                         <p></p>
-                        <p>{data2}</p>
+                        <p>{note}</p>
                     </div>
                 ):(
                     <h1>You haven't made any notes</h1>
