e6691d67e78283fda31fb7795c5e891815149e9f
pius <piusrestiantoro2@gmail.com>
2023-06-16 11:06:26 +0800

    note edit route added

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,6 +6,7 @@ import Signin from "./pages/signin/signin";
 import EditProfile from "./pages/profile/editProfile";
 import Notes from "./pages/notes/notes";
 import NotePreview from "./pages/notes/notePreview";
+import EditNote from "./pages/notes/noteEdit";
 
 function TheRouter() {
     
@@ -18,6 +19,7 @@ function TheRouter() {
                 <Route path="/profile/edit" element={<EditProfile/>}/>
                 <Route path="/notes" element={<Notes/>}/>
                 <Route path="/notes/preview/:id" element={<NotePreview/>}/>
+                <Route path="/note/edit/:id" element={<EditNote/>} />
             </Routes>
         </Router>
     )
