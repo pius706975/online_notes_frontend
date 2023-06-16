b3a25518d5bcfd7f0d639d41c07ff68f7db12ddc
pius <piusrestiantoro2@gmail.com>
2023-06-16 11:08:22 +0800

    note edit updated

--- frontend/src/pages/notes/noteEdit.jsx
+++ frontend/src/pages/notes/noteEdit.jsx
@@ -10,19 +10,70 @@ import Api from "../../helpers/api";
 import { useNavigate, useParams } from "react-router-dom";
 
 function EditNote() {
+    
+    const {isAuth} = useSelector((state)=>state.users)
+    const navigate = useNavigate()
+    const params = useParams()
+    const api = Api()
+    const [editNote, setEditNote] = useState([])
+    const [data, setData] = useState([])
+    
+    const onChangeInput = (event)=>{
+        event.preventDefault()
+        const tmpData = {...data}
+        tmpData[event.target.name] = event.target.value
+        setData(tmpData)
+        console.log(data)
+    }
+
+    const getNote = ()=>{
+        api.requests({
+            method: 'GET',
+            url: '/note/' + params.id
+        }).then((res)=>{
+            const {data} = res.data
+            setEditNote(data)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    const editData = ()=>{
+        api.requests({
+            method: 'PUT',
+            url: '/note/edit/' + params.id,
+            data: data
+        }).then((res)=>{
+            console.log(res)
+            navigate('/notes/preview/' + params.id)
+        }).catch((err)=>{
+            alert(err)
+        })
+    }
+
+    useEffect(()=>{
+        getNote()
+        setData({
+            ...data
+        })
+    }, [])
 
     return(
         <div className="edit-note-container">
             <NavbarCom/>
 
+            {isAuth ? (
                 <div class="jumbroton" id="myContainer">
-                    <input type="text" name="title" className="note-title" placeholder="Title"  required/> <br />
-                    <textarea className="notepad__textarea" name="note" placeholder="Your note . . ." /> <br />
+                    <input type="text" name="title" className="note-title" placeholder="Title" defaultValue={editNote.title} onChange={onChangeInput} required/> <br />
+                    <textarea className="notepad__textarea" name="note" placeholder="Your note . . ." defaultValue={editNote.note} onChange={onChangeInput} /> <br />
     
                     <div className="button">
-                        <button className="save-button" ><BsSave2/> Save</button>
+                        <button className="save-button" onClick={editData}><BsSave2/> Save</button>
                     </div>
                 </div>
+            ):(
+                window.location.reload(navigate('/notes'))
+            )}
 
             <FooterCom/>
         </div>
